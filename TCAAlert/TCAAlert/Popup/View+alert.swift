c727bbb05df2a20cd6aaf4c4cd356be9082ead85
mino <66459715+stealmh@users.noreply.github.com>
2024-12-10 17:28:33 +0900

    alertTwoButton View Modifier 제거

--- TCAAlert/TCAAlert/Popup/View+alert.swift
+++ TCAAlert/TCAAlert/Popup/View+alert.swift
@@ -31,28 +31,6 @@ extension View {
             )
         )
     }
-    
-    public func alertTwoButton<MenuAction: Equatable>(
-        _ item: Binding<Store<PopupAlertState<MenuAction>, MenuAction>?>
-    ) -> some View {
-        let store = item.wrappedValue
-        let state = store?.withState { $0 }
-        return self.alert(
-            item: Binding(
-                get: {
-                    state?.twoButtonConverted(
-                        send: { store?.send($0) },
-                        animation: { store?.send($0, animation: $1) }
-                    )
-                },
-                set: {
-                    if $0 == nil {
-                        item.transaction($1).wrappedValue = nil
-                    }
-                }
-            )
-        )
-    }
 }
 
 
