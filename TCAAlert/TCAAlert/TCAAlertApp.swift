8282ab85dd7cb394f2c505aac4bbbb1b971f83ff
mino <66459715+stealmh@users.noreply.github.com>
2024-12-10 11:01:41 +0900

    project init

---
+++ TCAAlert/TCAAlert/TCAAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAAlertApp.swift
+//  TCAAlert
+//
+//  Created by 김민호 on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
